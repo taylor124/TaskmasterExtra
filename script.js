3b1a9aeb7d7267de92a9ada86170333da9803637
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-01-15 14:02:56 -0500

    Feature: localStorage time variables
    localStorage now saves the specific span element with id's

--- script.js
+++ script.js
@@ -2,13 +2,32 @@ var saved = [];
 
 var currentDayEL = document.getElementById('currentDay');
 
-setInterval(function () { 
-    currentDayEL.textContent = moment().format('MMMM Do YYYY, h:mm:ss a'); 
+setInterval(function () {
+    currentDayEL.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
 }, 1000);
 
 
-for (var i = 0; )
+for (var i = 0; i < 9; i++) {
+    var timeBlockEL = document.createElement('div')
+    var timeBlockspan = document.createElement('span')
+    timeBlockspan.textContent = (i + 9) + ':00'
+    var timeBlocktextArea = document.createElement('textarea')
+    var timeBlockbtn = document.createElement('button')
+    timeBlockbtn.id = (i+9)
+    timeBlockbtn.classList.add('saveBtn')
+    timeBlockbtn.addEventListener('click', function (event) {
+        console.log(event.target.parentElement.id)
+        localStorage.setItem(event.target.parentElement.id,
+            event.target.parentElement.parentElement.children[1].value)
+    });
 
-document.getElementsByClassName('saveBtn', function () {
-    localStorage.setItem('saveP')
-});
+    var timeBlockIcon = document.createElement('i')
+    timeBlockIcon.textContent = 'save'
+    timeBlockbtn.appendChild(timeBlockIcon)
+    timeBlockEL.appendChild(timeBlockspan)
+    timeBlockEL.appendChild(timeBlocktextArea)
+    timeBlockEL.appendChild(timeBlockbtn)
+    document.querySelector('.container').appendChild(timeBlockEL)
+}
+
+console.log(moment().hours())
