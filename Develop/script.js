13fd5324ccdaa06b7dc8bc5590748fca0b594c14
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-01-15 12:57:43 -0500

    Feature: setInterval Time
    Added the ability to see the current day, time, and year while also updating it

---
+++ Develop/script.js
@@ -0,0 +1,14 @@
+var saved = [];
+
+var currentDayEL = document.getElementById('currentDay');
+
+setInterval(function () { 
+    currentDayEL.textContent = moment().format('MMMM Do YYYY, h:mm:ss a'); 
+}, 1000);
+
+
+for (var i = 0; )
+
+document.getElementsByClassName('saveBtn', function () {
+    localStorage.setItem('saveP')
+});
