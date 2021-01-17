096683c5002f5777d62154e8932effe66dd0f454
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-01-17 15:04:09 -0500

    Feature: localStorage done + moment
    LocalStorage now save over other tasks, and doesnt disappear when reloading page, moment  will now display past, present and future correctly

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-var saved = [];
-
 var currentDayEL = document.getElementById('currentDay');
 
 setInterval(function () {
     currentDayEL.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
 }, 1000);
 
-
+//for loop creates the span, textarea and save button 9 times
 for (var i = 0; i < 9; i++) {
-    var timeBlockEL = document.createElement('div')
-    var timeBlockspan = document.createElement('span')
-    timeBlockspan.textContent = (i + 9) + ':00'
-    var timeBlocktextArea = document.createElement('textarea')
-    var timeBlockbtn = document.createElement('button')
-    timeBlockbtn.id = (i+9)
-    timeBlockbtn.classList.add('saveBtn')
-    timeBlockbtn.addEventListener('click', function (event) {
-        console.log(event.target.parentElement.id)
-        localStorage.setItem(event.target.parentElement.id,
-            event.target.parentElement.parentElement.children[1].value)
+    var timeBlockEL = document.getElementById(''+ (i + 9))
+    timeBlockEL.childNodes[5].addEventListener('click', function (event) {
+        console.log(event.target.parentElement.parentElement.id)
+        localStorage.setItem(event.target.parentElement.parentElement.id,
+            event.target.parentElement.parentElement.childNodes[3].value)
     });
 
-    var timeBlockIcon = document.createElement('i')
-    timeBlockIcon.textContent = 'save'
-    timeBlockbtn.appendChild(timeBlockIcon)
-    timeBlockEL.appendChild(timeBlockspan)
-    timeBlockEL.appendChild(timeBlocktextArea)
-    timeBlockEL.appendChild(timeBlockbtn)
-    document.querySelector('.container').appendChild(timeBlockEL)
-}
+    /*const timeBlockclass = timeBlocktextArea.getAttribute('class')
+    console.log(timeBlockclass)*/
 
-console.log(moment().hours())
+    var localStoragefunction = localStorage.getItem(i+9)
+    console.log(localStoragefunction)
+    timeBlockEL.childNodes[3].textContent = localStoragefunction
+
+
+    if (moment().hours() === (i+9)){
+        timeBlockEL.childNodes[3].classList.add('present')
+    }
+    else if (moment().hours() > (i+9)){
+        timeBlockEL.childNodes[3].classList.add('past')
+    }
+    else {
+        timeBlockEL.childNodes[3].classList.add('future')
+    }
+    //document.querySelector('div').classList.add()
+
+}
+console.log(localStorage)
